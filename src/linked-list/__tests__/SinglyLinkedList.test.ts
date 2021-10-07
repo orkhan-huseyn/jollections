478bc6bdd0eeae919e23e7a42b48b0e73f0863bb
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2021-10-07 17:37:21 +0200

    test(linked-list): add test cases for singly linked list

--- src/linked-list/__tests__/SinglyLinkedList.test.ts
+++ src/linked-list/__tests__/SinglyLinkedList.test.ts
@@ -1,9 +1,66 @@
 import { SinglyLinkedList } from '../SinglyLinkedList';
 
 describe('SinglyLinkedList', () => {
+  let linkedList: SinglyLinkedList<number>;
+
+  beforeEach(() => {
+    linkedList = new SinglyLinkedList();
+  });
+
   it('creates linked list with empty head and tail', () => {
-    const linkedList = new SinglyLinkedList();
+    expect(linkedList.isEmpty()).toBe(true);
+    expect(linkedList.getSize()).toBe(0);
     expect(linkedList.first()).toBeNull();
     expect(linkedList.last()).toBeNull();
+  });
+
+  describe('when list is empty', () => {
+    it('properly adds new element to head', () => {
+      linkedList.addFirst(5);
+      expect(linkedList.isEmpty()).toBe(false);
+      expect(linkedList.first()).toBe(5);
+    });
+
+    it('properly adds new element to tail', () => {
+      linkedList.addLast(5);
+      expect(linkedList.isEmpty()).toBe(false);
+      expect(linkedList.last()).toBe(5);
+    });
+
+    it('properly removes element from head', () => {
+      expect(linkedList.getSize()).toBe(0);
+      expect(linkedList.isEmpty()).toBe(true);
+      expect(linkedList.removeFirst()).toBeNull();
+    });
+  });
+
+  describe('when list has one or more elements', () => {
+    it('properly adds new element to head', () => {
+      linkedList.addFirst(5);
+      linkedList.addFirst(4);
+      expect(linkedList.getSize()).toBe(2);
+      expect(linkedList.first()).toBe(4);
+      expect(linkedList.last()).toBe(5);
+    });
+
+    it('properly adds new element to tail', () => {
+      linkedList.addLast(5);
+      linkedList.addLast(6);
+      expect(linkedList.getSize()).toBe(2);
+      expect(linkedList.last()).toBe(6);
+      expect(linkedList.first()).toBe(5);
+    });
+
+    it('removes element from head and decreases size', () => {
+      linkedList.addFirst(5);
+      linkedList.addFirst(4);
+      expect(linkedList.getSize()).toBe(2);
+      expect(linkedList.first()).toBe(4);
+      expect(linkedList.last()).toBe(5);
+      linkedList.removeFirst();
+      expect(linkedList.getSize()).toBe(1);
+      expect(linkedList.first()).toBe(5);
+      expect(linkedList.last()).toBe(5);
+    });
   });
 });
