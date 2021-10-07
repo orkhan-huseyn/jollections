e8d7815cffa58e36de817d8ae3e033ad03866a34
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2021-10-07 21:18:27 +0200

    feat(linked-list): add removeLast method to singly linked list and add doc strings

--- src/linked-list/SinglyLinkedList.ts
+++ src/linked-list/SinglyLinkedList.ts
@@ -1,20 +1,33 @@
 export class SinglyLinkedListNode<T> {
   private element: T;
   private next: SinglyLinkedListNode<T> | null;
-
+  /**
+   * Creates new SinglyLinkedListNode
+   * @param {T} element value of list node
+   * @param {SinglyLinkedListNode<T>} next reference to the next node
+   */
   constructor(element: T, next: SinglyLinkedListNode<T>) {
     this.element = element;
     this.next = next;
   }
-
+  /**
+   * Returns the value of the node
+   * @returns {T} value of the node
+   */
   public getElement(): T {
     return this.element;
   }
-
+  /**
+   * Returns reference to the next node
+   * @returns {SinglyLinkedListNode<T> | null} reference to the next node
+   */
   public getNext(): SinglyLinkedListNode<T> | null {
     return this.next;
   }
-
+  /**
+   * Sets next of current node to provided node
+   * @param {SinglyLinkedListNode<T>} next next list node
+   */
   public setNext(next: SinglyLinkedListNode<T>): void {
     this.next = next;
   }
@@ -24,7 +37,10 @@ export class SinglyLinkedList<T> {
   private head: SinglyLinkedListNode<T> = null;
   private tail: SinglyLinkedListNode<T> = null;
   private size = 0;
-
+  /**
+   * Adds new node to the head of the list
+   * @param {T} element value to be added
+   */
   public addFirst(element: T): void {
     const newest = new SinglyLinkedListNode<T>(element, this.head);
     this.head = newest;
@@ -33,7 +49,10 @@ export class SinglyLinkedList<T> {
     }
     this.size++;
   }
-
+  /**
+   * Adds new node to the tail of the list
+   * @param {T} element value to be added
+   */
   public addLast(element: T): void {
     const newest = new SinglyLinkedListNode<T>(element, null);
     if (this.isEmpty()) {
@@ -44,7 +63,10 @@ export class SinglyLinkedList<T> {
     this.tail = newest;
     this.size++;
   }
-
+  /**
+   * Removes node from the head of the list
+   * @returns {T} value of first node
+   */
   public removeFirst(): T {
     if (this.isEmpty()) {
       return null;
@@ -57,22 +79,52 @@ export class SinglyLinkedList<T> {
     }
     return first;
   }
-
+  /**
+   * Removes node from the tail of the list
+   * @returns {T} value of the last node
+   */
+  public removeLast(): T {
+    if (this.isEmpty) {
+      return null;
+    }
+    let penultimate = this.head;
+    while (penultimate.getNext().getNext() !== null) {
+      penultimate = penultimate.getNext();
+    }
+    this.tail = penultimate;
+    const oldTail = penultimate.getNext();
+    penultimate.setNext(null);
+    this.size--;
+    return oldTail.getElement();
+  }
+  /**
+   * Returns size of the list
+   * @returns {number} size of the list
+   */
   public getSize(): number {
     return this.size;
   }
-
+  /**
+   * Returns true if list size is zero, false otherwise
+   * @returns {boolean} true if list is empty
+   */
   public isEmpty(): boolean {
     return this.size === 0;
   }
-
+  /**
+   * Returns value of the first node
+   * @returns {T} value of the first node
+   */
   public first(): T {
     if (this.isEmpty()) {
       return null;
     }
     return this.head.getElement();
   }
-
+  /**
+   * Returns value of the last node
+   * @returns {T} value of last node
+   */
   public last(): T {
     if (this.isEmpty()) {
       return null;
