816abec5aa1b55f550a1c319cab5dd60b7a386c7
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2021-10-24 20:28:49 +0200

    test(queue): add more test cases for priority queue

--- src/queue/__tests__/PriorityQueue.test.ts
+++ src/queue/__tests__/PriorityQueue.test.ts
@@ -1,5 +1,10 @@
 import { PriorityQueue } from '../PriorityQueue';
 
+interface Edge {
+  vertex: string;
+  weight: number;
+}
+
 describe('PriorityQueue', () => {
   it('throws and error when comparator function is not provided', () => {
     expect(() => new PriorityQueue<number>()).toThrowError();
@@ -11,6 +16,14 @@ describe('PriorityQueue', () => {
     );
     expect(queue.isEmpty()).toBe(true);
     expect(queue.size()).toBe(0);
+  });
+
+  it('return null for top element when queue is empty', () => {
+    const queue = new PriorityQueue<number>(
+      (a: number, b: number): number => a - b
+    );
+    expect(queue.peek()).toBeNull();
+    expect(queue.poll()).toBeNull();
   });
 
   it('implements min heap correctly', () => {
@@ -39,5 +52,18 @@ describe('PriorityQueue', () => {
     expect(queue.peek()).toBe(3);
     expect(queue.poll()).toBe(3);
     expect(queue.size()).toBe(2);
+  });
+
+  it('works properly with custom shapes', () => {
+    const queue = new PriorityQueue<Edge>(
+      (a: Edge, b: Edge): number => a.weight - b.weight
+    );
+    queue.add({ vertex: 'A', weight: 2 });
+    queue.add({ vertex: 'B', weight: 6 });
+    queue.add({ vertex: 'C', weight: 1 });
+    queue.add({ vertex: 'D', weight: 3 });
+    expect(queue.isEmpty()).toBe(false);
+    expect(queue.size()).toBe(4);
+    expect(queue.poll().vertex).toBe('C');
   });
 });
