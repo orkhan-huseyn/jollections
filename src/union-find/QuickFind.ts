93783b0184ecf321f2aa6939146bbfa62a908b84
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2021-10-25 17:15:57 +0200

    feat(union-find): tune quick find class

--- src/union-find/QuickFind.ts
+++ src/union-find/QuickFind.ts
@@ -3,20 +3,34 @@ export class QuickFind {
   private count: number;
   /**
    * Creates QuickFind class with given size
-   * @param {number} size size of the set
+   * @param {number} size size of the set, default size is 16
    */
-  constructor(size: number) {
+  constructor(size = 16) {
+    this.roots = new Array<number>(size);
     for (let i = 0; i < size; i++) {
       this.roots[i] = i;
     }
     this.count = size;
   }
   /**
+   * Validates that p is a valid index
+   * @param {number} p
+   * @returns {void}
+   */
+  private validate(p: number): void {
+    if (p < 0 || p >= this.roots.length) {
+      throw new Error(
+        'index ' + p + ' is not between 0 and ' + (this.roots.length - 1)
+      );
+    }
+  }
+  /**
    * Returns root of given node
    * @param {number} x arbitrary node
    * @returns {number} root of give node
    */
-  find(x: number): number {
+  public find(x: number): number {
+    this.validate(x);
     return this.roots[x];
   }
   /**
@@ -25,9 +39,9 @@ export class QuickFind {
    * @param {number} y node y
    * @returns {void}
    */
-  uion(x: number, y: number): void {
-    const rootX = this.roots[x];
-    const rootY = this.roots[y];
+  public uion(x: number, y: number): void {
+    const rootX = this.find(x);
+    const rootY = this.find(y);
     if (rootX !== rootY) {
       for (let i = 0; i < this.roots.length; i++) {
         if (this.roots[i] === rootY) {
@@ -43,14 +57,14 @@ export class QuickFind {
    * @param {number} y node y
    * @returns {boolean} whether the given nodes are connected
    */
-  isConnected(x: number, y: number): boolean {
+  public isConnected(x: number, y: number): boolean {
     return this.find(x) === this.find(y);
   }
   /**
    * Returns number of connected components
    * @returns {number} number of connected components
    */
-  getCount(): number {
+  public getCount(): number {
     return this.count;
   }
 }
