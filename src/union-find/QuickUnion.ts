2f236c9ef58d0e1efc575374865e0990518f014a
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2021-10-26 19:22:41 +0200

    feat(union-find): add quick union implementation

--- src/union-find/QuickUnion.ts
+++ src/union-find/QuickUnion.ts
@@ -1,1 +1,30 @@
-export class QuickUnion {}
+import { UnionFind } from './UnionFind';
+
+export class QuickUnion extends UnionFind {
+  /**
+   * Returns root of given node
+   * @param {number} x arbitrary node
+   * @returns {number} root of give node
+   */
+  public find(x: number): number {
+    this.validate(x);
+    while (x !== this.roots[x]) {
+      x = this.roots[x];
+    }
+    return x;
+  }
+  /**
+   * Connects nodes x and y with quick union strategy
+   * @param {number} x node x
+   * @param {number} y node y
+   * @returns {void}
+   */
+  public uion(x: number, y: number): void {
+    const rootX = this.find(x);
+    const rootY = this.find(y);
+    if (rootX !== rootY) {
+      this.roots[rootY] = rootX;
+      this.count--;
+    }
+  }
+}
